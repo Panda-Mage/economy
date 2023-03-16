9ac58c59cc4f0ab000ac2624af4c8a35076c80a5
magicroel <99012117+Panda-Mage@users.noreply.github.com>
2023-03-16 08:17:57 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <nav>
+            <p><a href="index.php">HOME</a></p>
+        </nav>
+        <form>
+            <select name="Klass">
+                <?php foreach(): ?>
+
+                <? endforeach; ?>
+            </select>
+        </form>
+    </body>
+</html>
