d4a2e9e9f2463c5e2eabeea19287ab19ed68a098
magicroel <99012117+Panda-Mage@users.noreply.github.com>
2023-03-17 13:59:45 +0100

    afbeelding.php

---
+++ afbeelding.php
@@ -0,0 +1,18 @@
+<?php 
+
+?>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="icon" href="foto's/custom_foto's/gosnapit.png">
+        <title>AFBEELDING</title>
+    </head>
+    <body>
+        <nav>
+            <a href="index.php"><p>HOME</p></a>
+            <a href="photos.php"><p>PHOTO'S</p></a>
+            <a href="winkelMandje.php"><img src="foto's/custom_foto's/winkelMandje.png"><p id="counter"><?php echo $counter ?></p></a> 
+        </nav>
+        <img src="">
+    </body>
+</html>
