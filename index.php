644b1244be899a6ffe1acd2c80f62ea81a583786
cedricvdbr <99012250+cedricvdbr@users.noreply.github.com>
2023-03-30 14:30:21 +0200

    eco

--- index.php
+++ index.php
@@ -14,32 +14,39 @@
         <title>Homepagina</title>
     </head>
     <body>
-        
-        <nav>
-            <a href="index.php"><p>HOME</p></a>
-            <a href="photos.php"><p>FOTO'S</p></a>
-            <a href="winkelMandje.php"><img src="foto's/custom_foto's/winkelMandje.png"><p id="counter"><?php echo $counter ?></p></a> 
-        </nav>
-        
-        <br>
-        <br>
-        <br>
-        <div class="container_content">
-            <div id="headtag">
-                <h1>PHOTOGRAFIE</h1>
-                <h2>GO SNAP IT</h2>
+    
+        <div class="main">
+
+            <nav>
+                <a href="index.php"><p>HOME</p></a>
+                <a href="photos.php"><p>FOTO'S</p></a>
+                <a href="winkelMandje.php"><img src="foto's/custom_foto's/winkelMandje.png"><p id="counter"><?php echo $counter ?></p></a> 
+            </nav>
+            
+            <br>
+            <br>
+            <br>
+
+            <div class="container_content">
+                <div id="headtag">
+                    <h1>PHOTOGRAFIE</h1>
+                    <h2>GO SNAP IT</h2>
+                </div>
+                <div class="container_text alignCenter">
+                    <h1>OVER ONS:</h1>
+                    <p>Wij zijn 4 enthousiaste junior ondernemers van de richting 6 Informaticabeheer en realiseren dit schooljaar de klasfoto's.</p>
+                    <p>Via deze website kunt u de klasfoto(s) van uzelf of uw zoon/dochter bestellen. Dit project hebben wij kunnen realiseren dankzij het  <a href="https://bezoek.go-atheneumoudenaarde.be/" target="_new">GO Atheneum Oudenaarde</a> en <a href="https://www.vlajo.org/" target="_new">Vlajo (Vlaamse Jonge Ondernemingen)</a></p>
+                </div>
             </div>
-            <div class="container_text alignCenter">
-                <h1>OVER ONS:</h1>
-                <p>Wij zijn 4 enthousiaste junior ondernemers van de richting 6 Informaticabeheer en realiseren dit schooljaar de klasfoto's.</p>
-                <p>Via deze website kunt u de klasfoto(s) van uzelf of uw zoon/dochter bestellen. Dit project hebben wij kunnen realiseren dankzij het  <a href="https://bezoek.go-atheneumoudenaarde.be/" target="_new">GO Atheneum Oudenaarde</a> en <a href="https://www.vlajo.org/" target="_new">Vlajo (Vlaamse Jonge Ondernemingen)</a></p>
+
+            <div class="footer">
+                <div class="verticalAlign">
+                    <img src="foto's/custom_foto's//button_onder_toezicht_van_Vlajovzw_HR.jpg" >
+                    <img src="foto's/custom_foto's/go-ao_logo.png">
+                </div>
             </div>
+
         </div>
-        <div class="footer">
-            <div class="verticalAlign">
-                <img src="foto's/custom_foto's//button_onder_toezicht_van_Vlajovzw_HR.jpg" >
-                <img src="foto's/custom_foto's/go-ao_logo.png">
-            </div>
-        </div>
+
     </body>
 </html>
