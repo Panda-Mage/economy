b25303798935410a01725b25c1bf8e11c44c0a12
cedricvdbr <cedric.vandenbranden@leerling.go-atheneumoudenaarde.be>
2023-04-05 14:30:10 +0200

    footer fix

--- index.php
+++ index.php
@@ -14,6 +14,12 @@
         <link rel="stylesheet" href="css/main.css">
         <link rel="icon" href="foto's/custom_foto's/gosnapit.png">
         <title>Homepagina</title>
+
+        <style>
+            .container_content {
+                height: 84%;
+            }
+        </style>
     </head>
     <body>
     
@@ -24,26 +30,24 @@
                 <a href="photos.php"><p>FOTO'S</p></a>
                 <a href="winkelMandje.php"><img src="foto's/custom_foto's/winkelMandje.png"><p id="counter"><?php echo $counter ?></p></a> 
             </nav>
-            
-            <br>
-            <br>
-            <br>
 
             <div class="container_content">
-                <div id="headtag">
-                    <h1>PHOTOGRAFIE</h1>
-                    <h2>GO SNAP IT</h2>
-                </div>
-                <div class="container_text alignCenter">
-                    <h1>OVER ONS:</h1>
-                    <p>Wij zijn 4 enthousiaste junior ondernemers van de richting 6 Informaticabeheer en realiseren dit schooljaar de klasfoto's.</p>
-                    <p>Via deze website kunt u de klasfoto(s) van uzelf of uw zoon/dochter bestellen. Dit project hebben wij kunnen realiseren dankzij het  <a href="https://bezoek.go-atheneumoudenaarde.be/" target="_new">GO Atheneum Oudenaarde</a> en <a href="https://www.vlajo.org/" target="_new">Vlajo (Vlaamse Jonge Ondernemingen)</a></p>
+                <div class="container_content_border">
+                    <div id="headtag">
+                        <h1>PHOTOGRAFIE</h1>
+                        <h2>GO SNAP IT</h2>
+                    </div>
+                    <div class="container_text alignCenter">
+                        <h1>OVER ONS:</h1>
+                        <p>Wij zijn 4 enthousiaste junior ondernemers van de richting 6 Informaticabeheer en realiseren dit schooljaar de klasfoto's.</p>
+                        <p>Via deze website kunt u de klasfoto(s) van uzelf of uw zoon/dochter bestellen. Dit project hebben wij kunnen realiseren dankzij het  <a href="https://bezoek.go-atheneumoudenaarde.be/" target="_new">GO Atheneum Oudenaarde</a> en <a href="https://www.vlajo.org/" target="_new">Vlajo (Vlaamse Jonge Ondernemingen)</a></p>
+                    </div>
                 </div>
             </div>
 
             <div class="footer">
                 <div class="verticalAlign">
-                    <img src="foto's/custom_foto's//button_onder_toezicht_van_Vlajovzw_HR.jpg" >
+                    <img src="foto's/custom_foto's/button_onder_toezicht_van_Vlajovzw_HR.jpg" >
                     <img src="foto's/custom_foto's/go-ao_logo.png">
                 </div>
             </div>
--- winkelMandje.php
+++ winkelMandje.php
@@ -34,41 +34,44 @@
                 <a href="winkelMandje.php"><img src="foto's/custom_foto's/winkelMandje.png"><p id="counter"><?php echo $counter ?></p></a> 
             </nav>
 
-            <?php if($counter != 0): ?>
-                <div id="tableDiv">
-                    <table>
-                        <tr class="header">
-                            <td><p>Item</p></td>
-                            <td><p>Prijs</p></td> 
-                        </tr>
-                        <?php $prijs = 0; ?>
-                        <?php foreach($_SESSION["items"] as $index =>$item): ?>
-                            <tr >
-                                <td style="background-color: <?php if($index%2 == 0){ echo "darkgray";}else{ echo "gray";} ?>;"><p><?php echo ($item); ?></p></td>
-                                <td style="background-color: <?php if($index%2 == 0){ echo "darkgray";}else{ echo "gray";} ?>;"><p><?php if(strrpos($item,"PAKKET")){ echo "€5,50"; $prijs = $prijs+5.5; } else{echo "€3,50"; $prijs = $prijs+3.5;} ?></p></td>
-                                <form method="post"><td style="background-color: transparent; cursor:grab;"><button onclick="this.form.submit()" style="background-color: transparent; color:transparent; border:none;"><img class="trash" src="foto's/custom_foto's/trashBin.png"></button><input type="text" name="row" value="<?php echo $index  ?>" class="invisible"></td></form>
+            <div class="container_content">
+
+                <?php if($counter != 0): ?>
+                    <div id="tableDiv">
+                        <table>
+                            <tr class="header">
+                                <td><p>Item</p></td>
+                                <td><p>Prijs</p></td> 
                             </tr>
-                        <?php endforeach; ?>
-                        <tr>
-                            <td style="background-color:red;"><p>TOTAAL</p></td>
-                            <td style="background-color:darkred;"><p><?php if (strpos($prijs, '.') > 0) {echo '€'.$prijs."0";} else{ echo '€'.$prijs.".00";} $_SESSION["prijs"] = $prijs;?></p></td>
-                        </tr>
-                        <tr style="background-color: transparen;">
-                            <td></td>
-                            <form method="post"><td><input type="submit" name="betalen" value="BESTELLEN"></td></form>
-                        </tr>
-                    </table>
-                </div>
+                            <?php $prijs = 0; ?>
+                            <?php foreach($_SESSION["items"] as $index =>$item): ?>
+                                <tr >
+                                    <td style="background-color: <?php if($index%2 == 0){ echo "darkgray";}else{ echo "gray";} ?>;"><p><?php echo ($item); ?></p></td>
+                                    <td style="background-color: <?php if($index%2 == 0){ echo "darkgray";}else{ echo "gray";} ?>;"><p><?php if(strrpos($item,"PAKKET")){ echo "€5,50"; $prijs = $prijs+5.5; } else{echo "€3,50"; $prijs = $prijs+3.5;} ?></p></td>
+                                    <form method="post"><td style="background-color: transparent; cursor:grab;"><button onclick="this.form.submit()" style="background-color: transparent; color:transparent; border:none;"><img class="trash" src="foto's/custom_foto's/trashBin.png"></button><input type="text" name="row" value="<?php echo $index  ?>" class="invisible"></td></form>
+                                </tr>
+                            <?php endforeach; ?>
+                            <tr>
+                                <td style="background-color:red;"><p>TOTAAL</p></td>
+                                <td style="background-color:darkred;"><p><?php if (strpos($prijs, '.') > 0) {echo '€'.$prijs."0";} else{ echo '€'.$prijs.".00";} $_SESSION["prijs"] = $prijs;?></p></td>
+                            </tr>
+                            <tr style="background-color: transparen;">
+                                <td></td>
+                                <form method="post"><td><input style="margin-top: 1em" type="submit" name="betalen" value="BESTELLEN"></td></form>
+                            </tr>
+                        </table>
+                    </div>
 
-            <?php else:?>
+                <?php else:?>
 
-                <br>
-                <h1>SORRY</h1>
-                <h1>U WINKELMANDJE IS LEEG</h1>
-                <h1>:(</h1>
+                    <h1 style="padding-top: 8vh;">SORRY</h1>
+                    <h1>U WINKELMANDJE IS LEEG</h1>
+                    <h1>:(</h1>
 
-            <?php endif; ?>
-            
+                <?php endif; ?>
+
+            </div>
+
             <div class="footer">
                 <div class="verticalAlign">
                     <img src="foto's/custom_foto's//button_onder_toezicht_van_Vlajovzw_HR.jpg" >
